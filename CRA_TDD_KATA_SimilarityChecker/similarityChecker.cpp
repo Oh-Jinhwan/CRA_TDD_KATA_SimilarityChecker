c24a7dd8c84e20023ce124e83c85cb76fe0d2850
jinhwan.oh <ognan98@gmail.com>
2023-06-02 14:22:27 +0900

    등장 알파벳 검사 구현

--- CRA_TDD_KATA_SimilarityChecker/similarityChecker.cpp
+++ CRA_TDD_KATA_SimilarityChecker/similarityChecker.cpp
@@ -6,6 +6,7 @@ class SimilarityChecker
 public:
 	const int MAX_LENGTH_POINT = 60;
 	const int MIN_LENGTH_POINT = 0;
+	const int MAX_ALPHABET_POINT = 40;
 
 	int checkSimilarity(string str1, string str2)
 	{
@@ -19,10 +20,16 @@ public:
 		return getLengthPoint(str1.size(), str2.size());
 	}
 
+
 	int checkAlphabet(string str1, string str2)
 	{
-		// to do
-		return 0;
+		int sameCnt = 0;
+		int totalCnt = 0;
+
+		getSameAndTotalCount(str1, str2, sameCnt, totalCnt);
+
+		if (totalCnt == 0) return 0;
+		return sameCnt * MAX_ALPHABET_POINT / totalCnt;
 	}
 
 private :
@@ -41,5 +48,26 @@ private :
 	{
 		if (str1Size > str2Size) return (MAX_LENGTH_POINT * 2) - (str1Size * MAX_LENGTH_POINT / str2Size);
 		return MAX_LENGTH_POINT * 2 - (str2Size * MAX_LENGTH_POINT / str1Size);
+	}
+
+	void getSameAndTotalCount(string str1, string str2, int& sameCnt, int& totalCnt)
+	{
+		for (char alphabet = 'A'; alphabet <= 'Z'; alphabet++) {
+			int found = 0;
+			for (char ch1 : str1) {
+				if (alphabet == ch1) {
+					found++;
+					break;
+				}
+			}
+			for (char ch2 : str2) {
+				if (alphabet == ch2) {
+					found++;
+					break;
+				}
+			}
+			if (found >= 1) totalCnt++;
+			if (found == 2) sameCnt++;
+		}
 	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -7,3 +7,19 @@ TEST(TestCaseName, LengthCheck) {
   EXPECT_EQ(20, SimilarityChecker().checkLength("AAABB", "BAA"));
   EXPECT_EQ(30, SimilarityChecker().checkLength("AA", "AAA"));
 }
+
+TEST(TestCaseName, AlphabetCheck) {
+	EXPECT_EQ(40, SimilarityChecker().checkAlphabet("ASD", "DSA"));
+	EXPECT_EQ(0, SimilarityChecker().checkAlphabet("A", "BB"));
+	EXPECT_EQ(40, SimilarityChecker().checkAlphabet("AAABB", "BA"));
+	EXPECT_EQ(20, SimilarityChecker().checkAlphabet("AA", "AAE"));
+}
+
+TEST(TestCaseName, SimilarityCheck) {
+	EXPECT_EQ(100, SimilarityChecker().checkSimilarity("ASD", "DSA"));
+	EXPECT_EQ(0, SimilarityChecker().checkSimilarity("A", "BB"));
+	EXPECT_EQ(60, SimilarityChecker().checkSimilarity("AAABB", "BAA"));
+	EXPECT_EQ(40, SimilarityChecker().checkSimilarity("AAABB", "BA"));
+	EXPECT_EQ(70, SimilarityChecker().checkSimilarity("AA", "AAA"));
+	EXPECT_EQ(50, SimilarityChecker().checkSimilarity("AA", "AAE"));
+}
